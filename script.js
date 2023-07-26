8b6b243630fc8cbca7014a8b4f2f5448c4ce891c
cyto-anu <anu.cyto@gmail.com>
2023-07-26 18:38:12 -0400

    playRound function added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice() {
-    randomNumber=(Math.random())
+
+randomNumber=(Math.random())
 
     if (randomNumber < (1/3)) {
         return "rock"
@@ -10,4 +11,46 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+
+
+function playRound (playerSelection, computerSelection) {
+
+    if (playerSelection === "rock" && computerSelection === "rock"){
+        return "Its a tie! you both choose rock";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You lose! paper beats rock";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! rock beats scissors";
+    }
+
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! paper beats rock";
+    }
+    else if (playerSelection === "paper" && computerSelection === "paper"){
+        return "Its a tie! you both choose paper";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "You lose! scissors beats paper";
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return "You lose! rock beats scissors";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! scissors beats paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "scissors"){
+        return "Its a tie! you both choose scissors";
+    }
+
+}
+
+function game() {
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(playRound(playerSelection, computerSelection))
+}
